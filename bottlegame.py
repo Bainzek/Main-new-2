ce3ea4930f52c8afa3425c8ab8f699da8ef6f92b
Bainzek <kbainze@gmail.com>
2025-07-08 12:07:32 +0200

    Initial commit

---
+++ bottlegame.py
@@ -0,0 +1,32 @@
+# 99 bottles of beer on the wall
+num_bottles = 99 
+while num_bottles > 0 :
+    if num_bottles == 1:
+           print(f"{num_bottles} bottle of beer on the wall, {num_bottles} bottle of beer.")
+           print("Take one down, pass it around, no more bottles of beer on the wall.")
+    else:
+           print(f"{num_bottles} bottles of beer on the wall, {num_bottles} bottles of beer.")
+    num_bottles = num_bottles - 1
+    if num_bottles == 0:
+            print("No more bottles of bear on the wall,no more bottles of beer.")
+            print(f"go to the store and buy some more,99 bottles of beer on the wall.")
+    else:
+         print(f"Take one down ,pass it around , {num_bottles} bottles of beer on the wall")
+       
+
+
+        
+        
+
+
+
+
+
+    
+    
+
+        
+        
+
+
+
---
+++ lists2.py
@@ -0,0 +1,12 @@
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+# you can access items in a list using their index
+print(my_list[0])  # prints the first item in the list
+print(my_list[1])  # prints the second item in the list
+
+# you can also use negative indexing to access items from the end of the list
+print(my_list[::-1])  # prints the last item in the list
+print(my_list[1::])
+
+my_list2 = [-200,20,-5,45,96,88,-40,12,46,21]
+print(my_list2[::-1])
